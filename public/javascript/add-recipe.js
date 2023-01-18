28a440e777aa620715ef452fa93c6be608a5c3d4
Gonzalo <ggarciarivera09@gmail.com>
2023-01-18 00:10:51 -0500

    update 2

--- public/javascript/add-recipe.js
+++ public/javascript/add-recipe.js
@@ -17,18 +17,19 @@
 <input type="text" id="i7" class="ing_name" value="Pickles"/>,
 
 
-async function postRecipe(event) {
+async function postRecipe(table, data, event) {
     event.preventDefault();
     const meal_title = document.querySelector('input[name="meal-title"]').value;
-    // const meal_country = document.querySelector('input[name="meal-country"]').value;
-    // const ing_id = //document.querySelector('input[name="ing-id"]').value;
-    // const qty_amount = document.querySelector('input[name="qty-amount"]').value;
-    const meal_instructions = document.querySelector('input[name="meal-instructions"]').value;
+    
+    // const qty_value = //document.querySelector('input[name="ing-id"]').value;
+    const qty_value = document.querySelector('input[name="qty_value"]').value;
+    const user_id = document.querySelector('input[name="user_id"]').value;
     const response = await fetch(`/api/recipes`, {
         method: 'POST',
         body: JSON.stringify({
-            title,
-            post_url
+            meal_id,
+            qty_value,
+            user_id
             }),
         headers: {
             'Content-Type': 'application/json'
